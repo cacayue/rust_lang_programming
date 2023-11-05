583b19ccd4e943bf8907c2d234cc143582f293a8
mafutian <a15453606@outlook.com>
2023-11-05 16:50:48 +0800

    feat: 函数的生命周期范围

--- ref_safe/src/main.rs
+++ ref_safe/src/main.rs
@@ -1,14 +1,18 @@
 fn main() {
-    println!("Hello, world!");
     error_demo();
     success_demo();
+
+    let x = 1;
+    g(&x);
+    // x的生命周期不能和static一样长
+    // _g1(&x);
 }
 
 fn error_demo(){
-    let r;
+    let _r;
     {
         let x = 1;
-        r = &x;
+        _r = &x;
     }
     // 超出x的作用域，调用出错
     //assert_eq!(*r, 1);
@@ -21,4 +25,13 @@ fn success_demo(){
         let r  = &x;
         assert_eq!(*r, 1);
     }
+}
+
+
+fn g<'a>(p: &'a i32){
+    println!("{}", p);
+}
+
+fn _g1(p: &'static i32){
+    println!("{}", p);
 }
