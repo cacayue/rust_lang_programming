e3459caeb8ef5d10863483caf62180f5e2e3df77
mafutian <a15453606@outlook.com>
2023-11-05 15:32:24 +0800

    移动的例外：COPY类型

--- move_learn/src/main.rs
+++ move_learn/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     }
     // 不允许使用转移掉所有权的值
     // println!("{:?}", v);
+    println!(r###"ownership Move [println!("", v)] is error"###);
 
     let v1 = vec!["101".to_string(),"102".to_string(),"103".to_string()];
     // 深拷贝值使用，不动用之前的所有权
@@ -13,7 +14,14 @@ fn main() {
         s.push('!');
         println!("{}", s);
     }
-    println!("{:?}", v1);
+    println!("CLONE complex Type{:?}", v1);
 
-    
+    // copy类型
+    let v2 = vec![101,102,103];
+    // 整数等简单类型直接会Copy，所有权还在自身
+    for s in &v2 {
+        let p = s.to_string() + "!";
+        println!("{}", p);
+    }
+    println!("COPY ---- {:?}", v2);
 }
